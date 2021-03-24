a2d267998a55589813a6d9eb8aafc085cf16e0d4
espere-papillon <marinabov007@gmail.com>
2021-03-24 23:58:17 +0300

    feat: update page dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./Dialogs.module.css";
 
 export const Dialogs = (props: any) => {
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogsItem}>
-                <div className={styles.dialog + ' ' + styles.active}>Marina</div>
-                <div className={styles.dialog}>Karina</div>
-                <div className={styles.dialog}>Stas</div>
+                <div className={styles.dialog + ' ' + styles.active}><NavLink to={"dialogs/1"}>Marina</NavLink></div>
+                <div className={styles.dialog}><NavLink to={"dialogs/2"}>Karina</NavLink></div>
+                <div className={styles.dialog}><NavLink to={"dialogs/3"}>Stas</NavLink></div>
             </div>
             <div className={styles.messages}>
                 <div className={styles.message}>Hi</div>
