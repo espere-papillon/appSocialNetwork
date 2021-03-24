0f436560901381ecfd015761eb5f149b2c1ab597
espere-papillon <marinabov007@gmail.com>
2021-03-24 23:43:20 +0300

    feat: update page dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,16 @@ import styles from "./Dialogs.module.css";
 
 export const Dialogs = (props: any) => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItem}>
+                <div className={styles.dialog + ' ' + styles.active}>Marina</div>
+                <div className={styles.dialog}>Karina</div>
+                <div className={styles.dialog}>Stas</div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi</div>
+                <div className={styles.message}>Hello</div>
+            </div>
         </div>
     )
 }
