2544386baa77cd6f481d8c3de4675086fedc89ef
espere-papillon <marinabov007@gmail.com>
2021-04-04 22:32:47 +0300

    feat: add lesson 37

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
-import {rerenderEntireTree} from "./render";
-import {state} from "./redax/state";
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {StateType, store} from './redax/state';
 
-rerenderEntireTree(state);
+
+export const rerenderEntireTree = (state: StateType) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state}
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}
+                 addMessage={store.addMessage.bind(store)}
+                 updateNewMessageText={store.updateNewMessageText.bind(store)} />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+rerenderEntireTree(store.getState())
+
+store.subscribe(rerenderEntireTree);
+
+
+
 
