3056ad37848aa285451b9d1a7a85281aced2211c
espere-papillon <marinabov007@gmail.com>
2021-07-19 23:05:19 +0300

    feat: add app container

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import {store} from './redux/redux-store';
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {AppContainer} from "./AppContainer";
 
 
 export const rerenderEntireTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <BrowserRouter>
-                    <App/>
-                </BrowserRouter>
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
+    ReactDOM.render(<AppContainer/>, document.getElementById('root'));
 }
 rerenderEntireTree();
 store.subscribe(rerenderEntireTree);
