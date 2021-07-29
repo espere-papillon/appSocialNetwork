201dc0f9a7381083222936fa251110ea5d71156c
espere-papillon <marinabov007@gmail.com>
2021-07-29 19:50:09 +0300

    Updates

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import {AppContainer} from "./AppContainer";
-
-test('renders without crashing', () => {
-  render(<AppContainer />);
-  const linkElement = screen.getByText(/profile/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/components/Dialogs/Message/Message.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "../Dialogs.module.css";
-import {MessageType} from "../../../redux/dialogs-reducer";
-
-export const Message: React.FC<MessageType> = (props) => {
-    return (
-        <div className={styles.message}>{props.message}</div>
-    )
-}
